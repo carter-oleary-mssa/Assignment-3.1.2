191499e109f23705ff21ba45e6b36d3ad31182ae
carter-oleary <carter.m.oleary@outlook.com>
2025-04-21 11:17:37 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int[] years = { 2016, 2018 };
+foreach(int year in years)
+{
+    if (IfYearIsLeap(year))
+        Console.WriteLine($"The year {year} is a leap year.");
+    else
+        Console.WriteLine($"The year {year} is not a leap year");
+}
+
+bool IfYearIsLeap(int  year)
+{
+    return year % 4 == 0;
+}
